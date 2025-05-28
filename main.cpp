6a5679fae261c32ac87fc4d5f3baabd003698baf
KristinaYuan <Kristina_0418@163.com>
2025-05-28 21:32:50 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,24 @@
 #include "mainwindow.h"
 #include "backend.h"
 #include <QApplication>
+#include <QLocale>
+#include <QTranslator>
 #include "networkvisualizer.h"
 #include "layerblockitem.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QTranslator translator;
+    const QStringList uiLanguages = QLocale::system().uiLanguages();
+    for (const QString &locale : uiLanguages) {
+        const QString baseName = "CodeWings_" + QLocale(locale).name();
+        if (translator.load(":/i18n/" + baseName)) {
+            a.installTranslator(&translator);
+            break;
+        }
+    }
     /*
     MainWindow w;
     w.show();
