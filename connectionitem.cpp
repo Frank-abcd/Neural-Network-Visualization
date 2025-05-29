12032fb6a09ecfa8e2e9ec21b6c112d9c6096fbc
yyxuanyao <yaoyyxuan@126.com>
2025-05-29 19:59:16 +0800

    Update connectionitem.cpp

--- connectionitem.cpp
+++ connectionitem.cpp
@@ -1,15 +1,22 @@
+// connectionitem.cpp
 #include "connectionitem.h"
+#include "colorthememanager.h"
 #include <QPen>
-#include "colorgroup.h"
 
-ConnectionItem::ConnectionItem(const QPointF& from, const QPointF& to, double weight) {
+ConnectionItem::ConnectionItem(const QPointF& from, const QPointF& to, double weight)
+    : m_weight(weight) {
     setLine(QLineF(from, to));
-
-    QPen pen;
-    // 通过静态方法获取当前主题
-    const ColorTheme& theme = ColorGroup::current();
-    pen.setColor(weight > 0.5 ? theme.connectionHighWeight : theme.connectionLowWeight);
-    pen.setWidthF(0.1 + weight * 1.9);
-    setPen(pen);
+    updateColor();  // 初始化颜色
     setZValue(0);
+}
+
+void ConnectionItem::updateColor() {
+    const ColorTheme& theme = ColorThemeManager::currentTheme();
+    QPen pen;
+    pen.setColor(m_weight > 0.5 ? theme.connectionHighWeight : theme.connectionLowWeight);
+    pen.setWidthF(0.1 + m_weight * 1.9);
+    setPen(pen);
+}
+
+
 }
