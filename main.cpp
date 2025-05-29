729f9d6ff1af4ac7732f1f108383b2645997c1fa
yyxuanyao <yaoyyxuan@126.com>
2025-05-29 22:13:14 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -31,6 +31,7 @@ int main(int argc, char *argv[])
     layers.append(l4);
 
     NetworkVisualizer* visualizer = new NetworkVisualizer();
+    visualizer->applyColorTheme("Ocean");
     visualizer->createNetwork(layers);
     visualizer->show();
 
