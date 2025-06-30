e377df2cd8d97e3669b0a8d392eaa48a13cb788f
yyxuanyao <yaoyyxuan@126.com>
2025-06-30 13:22:44 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,8 +4,7 @@
 #include <QLocale>
 #include <QTranslator>
 #include "networkvisualizer.h"
-#include "layerblockitem.h"
-
+#include "layerblockitem.h
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
@@ -19,24 +18,7 @@ int main(int argc, char *argv[])
             break;
         }
     }
-    /*
-    // 构造一些测试数据
-    QList<NeuralLayer> layers;
-    NeuralLayer l1; l1.layerType = "input"; l1.inputSize = 18; l1.neurons = 18; l1.activationFunction = " ";
-    NeuralLayer l2; l2.layerType = "hidden layer"; l2.inputSize = 18; l2.neurons = 10; l2.activationFunction = "linear";
-    NeuralLayer l3; l3.layerType = "hidden layer"; l3.inputSize = 18; l3.neurons = 10; l3.activationFunction = "linear";
-    NeuralLayer l4; l4.layerType = "output"; l4.inputSize = 18; l4.neurons = 1; l4.activationFunction = "softmax";
-    layers.append(l1);
-    layers.append(l2);
-    layers.append(l3);
-    layers.append(l4);
 
-    NetworkVisualizer* visualizer = new NetworkVisualizer();
-    visualizer->applyColorTheme("Ocean");
-    visualizer->createNetwork(layers);
-    visualizer->show();
-
-*/
 
     MainWindow w;
     MainWindow::setInstance(&w);
